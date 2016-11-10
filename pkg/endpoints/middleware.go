32ade29965521ee6ad288f05c4a7b6f76aac0f14
Peter Bourgon <peter@bourgon.org>
2016-11-10 15:25:30 -0800

    Continue

--- pkg/endpoints/middleware.go
+++ pkg/endpoints/middleware.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/metrics"
 	"golang.org/x/net/context"
 )
@@ -19,6 +20,21 @@ func InstrumentingMiddleware(duration metrics.Histogram) endpoint.Middleware {
 
 			defer func(begin time.Time) {
 				duration.With("success", fmt.Sprint(err == nil)).Observe(time.Since(begin).Seconds())
+			}(time.Now())
+			return next(ctx, request)
+
+		}
+	}
+}
+
+// LoggingMiddleware returns an endpoint middleware that logs the
+// duration of each invocation, and the resulting error, if any.
+func LoggingMiddleware(logger log.Logger) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+
+			defer func(begin time.Time) {
+				logger.Log("transport_error", err, "took", time.Since(begin))
 			}(time.Now())
 			return next(ctx, request)
 
