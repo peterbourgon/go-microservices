82e00b8cc2e954da9a9737cb95416905efa8e2b1
Peter Bourgon <peter@bourgon.org>
2016-11-10 15:14:49 -0800

    Continue

--- cmd/addsvc/main.go
+++ cmd/addsvc/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-kit/kit/metrics"
+	"github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/net/context"
 
 	"github.com/peterbourgon/go-microservices/pkg/endpoints"
@@ -17,12 +20,37 @@ func main() {
 	addr := flag.String("addr", ":8080", "HTTP listen address")
 	flag.Parse()
 
-	svc := service.New(log.New(os.Stderr, "", log.LstdFlags))
-	eps := endpoints.New(svc)
-	mux := addhttp.NewHandler(context.Background(), eps)
+	// Our metrics are dependencies, here we create them.
+	var ints, chars metrics.Counter
+	{
+		// Business level metrics.
+		ints = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "peterbourgon",
+			Subsystem: "addsvc",
+			Name:      "integers_summed",
+			Help:      "Total count of integers summed via the Sum method.",
+		}, []string{})
+		chars = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "peterbourgon",
+			Subsystem: "addsvc",
+			Name:      "characters_concatenated",
+			Help:      "Total count of characters concatenated via the Concat method.",
+		}, []string{})
+	}
+	var duration metrics.Histogram
+	{
+		// Transport level metrics.
+		duration = prometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "peterbourgon",
+			Subsystem: "addsvc",
+			Name:      "request_duration_seconds",
+			Help:      "Request duration in seconds.",
+		}, []string{"method", "success"})
+	}
 
-	// There's no magic here. Just construction, passing deps as params.
-	// Everything is explcit. Hopefully a breath of fresh air!
+	svc := service.New(log.New(os.Stderr, "", log.LstdFlags), ints, chars)
+	eps := endpoints.New(svc, duration)
+	mux := addhttp.NewHandler(context.Background(), eps)
 
 	log.Printf("listening on %s", *addr)
 	log.Fatal(http.ListenAndServe(*addr, mux))
