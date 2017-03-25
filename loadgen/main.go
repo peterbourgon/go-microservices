9fc56cd4f4d2f0c01ee66192c221699fced31ae6
Peter Bourgon <peter@bourgon.org>
2017-03-25 14:11:32 +0100

    WIP

--- loadgen/main.go
+++ loadgen/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -11,30 +10,40 @@ import (
 )
 
 func main() {
-	requests := []*http.Request{
-		mustNewRequest("GET", "http://addsvc.default.svc.cluster.local/sum", strings.NewReader(`{"a":123,"b":456}`)),
-		mustNewRequest("GET", "http://addsvc.default.svc.cluster.local/concat", strings.NewReader(`{"a":"hello","b":"world"}`)),
-		mustNewRequest("GET", "http://stringsvc.default.svc.cluster.local/uppercase", strings.NewReader(`{"s":"blep"}`)),
-		mustNewRequest("GET", "http://stringsvc.default.svc.cluster.local/count", strings.NewReader(`{"s":"🍔"}`)),
+	requests := []struct {
+		method string
+		url    string
+		body   string
+	}{
+		{"GET", "http://addsvc.default.svc.cluster.local/sum", `{"a":123,"b":456}`},
+		{"GET", "http://addsvc.default.svc.cluster.local/concat", `{"a":"hello","b":"world"}`},
+		{"GET", "http://stringsvc.default.svc.cluster.local/uppercase", `{"s":"blep"}`},
+		{"GET", "http://stringsvc.default.svc.cluster.local/count", `{"s":"🍔"}`},
 	}
 	for begin := range time.Tick(time.Second) {
-		req := requests[rand.Intn(len(requests))]
-		resp, err := http.DefaultClient.Do(req)
+		r := requests[rand.Intn(len(requests))]
+
+		req, err := http.NewRequest(r.method, r.url, strings.NewReader(r.body))
 		if err != nil {
-			log.Print(err)
+			log.Printf("NewRequest: %v", err)
 			continue
 		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Printf("DefaultClient.Do(%s %s): %v", r.method, r.url, err)
+			continue
+		}
+
 		body, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		bodyStr := strings.TrimSpace(string(body))
-		log.Printf("%s %s: %s (%s)", req.Method, req.URL.String(), bodyStr, time.Since(begin))
+		log.Printf(
+			"%s %s: %s: %s (%s)",
+			req.Method, req.URL.String(),
+			resp.Status,
+			bodyStr,
+			time.Since(begin),
+		)
 	}
-}
-
-func mustNewRequest(method, urlStr string, body io.Reader) *http.Request {
-	req, err := http.NewRequest(method, urlStr, body)
-	if err != nil {
-		panic(err)
-	}
-	return req
 }
